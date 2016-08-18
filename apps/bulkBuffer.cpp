35d330bbd6771984155daafa1971c789cfa5caee
paul <mig_29sk@163.com>
2016-08-18 09:13:13 +0800

    bulkBuffer

---
+++ apps/bulkBuffer.cpp
@@ -0,0 +1,54 @@
+#include "bulkBuffer.h"
+/**
+ * @brief pushPacketsToBuf 
+ * 将数据包移入session id下的缓存
+ * @param {interge} sId
+ * @param {bulkPacket} packet
+ */
+void bulkBuffer::pushPacketsToBuf(int sId, bulkPacket& packet)
+{
+	_buffers[sId]->push_back(packet);
+}
+
+/**
+ * @brief pullPacketsFromBuf 
+ * 将数据包移出session id下的缓存
+ * @param {interge} sId
+ * @param {bulkPacket} packet
+ *
+ * @return {slist<bulkPacket>}
+ */
+slist<bulkPacket>* bulkBuffer::pullPacketsFromBuf(int sId, int num)
+{
+	slist<bulkPacket>* pPackets = new slist<bulkPacket>();
+	int count = 0;
+	while (!_buffers[sId]->empty() && count < num) {
+		bulPacket& packet = _buffers[sId]->front();
+		pPackets->push_back(packet);
+		_buffers[sId]->pop_front();
+		count++;
+	}
+	return pPackets;
+}
+
+/**
+ * @brief getPacketsAmount 
+ * 获得session id下的数据包数量
+ * @param {interge} sId
+ *
+ * @return {interge}
+ */
+int bulkBuffer::getPacketsAmount(int sId)
+{
+	return _buffers[sId]->size();
+}
+
+/**
+ * @brief getBuffers 
+ * 获得缓存buffers
+ * @return {vector<slist<bulkPacket>* >}
+ */
+vector<slist<bulkPacket>* >& bulkBuffer::getBuffers()
+{
+	return _buffers;
+}
---
+++ apps/bulkBuffer.h
@@ -0,0 +1,25 @@
+#ifndef _BULKBUFFER_H_
+#define _BULKBUFFER_H_
+#define MAXSESSION 20
+#include <ext/slist>
+#include "bulkPacket.h"
+using namespace __gnu_cxx;
+class bulkBuffer
+{
+	private:
+		//_buffers: session id对应缓存队列
+		vector<slist<bulkPacket>* > _buffers; 
+	public:
+		bulkBuffer() {
+			_buffers.resize(MAXSESSION);
+			vector<slist<bulkPacket>* >::iterator iter;
+			for (iter = _buffers.begin(); iter != _buffers.end(); iter++) {
+				*iter = new slist<bulkPacket>();
+			}
+		}
+		void pushPacketsToBuf(int sId, bulkPacket& packet);
+		slist<bulkPacket>* pullPacketsFromBuf(int sId, int num);
+		int getPacketsAmount(int sId);
+		vector<slist<bulkPacket>* >& getBuffers();
+};
+#endif

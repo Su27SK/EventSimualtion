0633761e1e1619dc31bbbdce798d3b3c1440d8f5
paul <mig_29sk@163.com>
2016-08-25 23:45:28 +0800

    test

--- BulkTest.cpp
+++ BulkTest.cpp
@@ -7,6 +7,9 @@ int main(int argc, char* argv[])
 {
 	Graph* graph = Graph::importGraph("../Bulk_Config_File/graph.txt");
 	bulkBackPressure* backPressure = new bulkBackPressure(graph);
+	backPressure->setSession(1, 4);
+	backPressure->setSession(2, 5);
+	backPressure->handle();
 	cout<<"Hello World"<<endl;
 	return 0;
 }

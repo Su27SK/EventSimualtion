b7319f44e72eca991c78f917cb7af48227f06e2f
paul <mig_29sk@163.com>
2016-09-11 02:13:44 +0800

    Test fordFulkersion

--- BulkTest.cpp
+++ BulkTest.cpp
@@ -4,6 +4,7 @@
 #include "bulkBackPressure.h"
 #include "flowNetwork.h"
 #include "fordFulkersion.h"
+#include "overlaySimulation.h"
 using namespace std;
 int main(int argc, char* argv[])
 {
@@ -11,16 +12,20 @@ int main(int argc, char* argv[])
 	Graph* graph = Graph::importGraph("../Bulk_Config_File/world.txt");
 	flowNetwork* pNetwork = new flowNetwork(graph);
 	fordFulkersion::FordFulkersion(*pNetwork, 1, 8);
-	cout<<fordFulkersion::getValue()<<endl;
+	cout<<"value:"<<fordFulkersion::getValue()<<endl;
 	//bulkBackPressure* backPressure = new bulkBackPressure(graph);
 	//backPressure->setSession(1, 1, 4, 4);
 	//backPressure->setSession(2, 1, 4, 5);
 	//backPressure->handle();
-	slist<bulkFlow*>* pFlow = pNetwork->getAdj(1);
-	slist<bulkFlow*>::iterator iter = pFlow->begin();
-	for (; iter != pFlow->end(); iter++) {
-		cout<<(*iter)->getCapacity()<<endl;
-	}
+	bulkOverlay* overlay = new bulkOverlay(graph);
+	overlaySimulation* pSimulation = new overlaySimulation(overlay);
+	//slist<bulkFlow*>* pFlow = pNetwork->getAdj(1);
+	//slist<bulkFlow*>::iterator iter = pFlow->begin();
+	//for (; iter != pFlow->end(); iter++) {
+		//cout<<(*iter)->getCapacity()<<endl;
+	//}
+	//pSimulation->start();
+	//pSimulation->run();
 	cout<<"Hello World"<<endl;
 	return 0;
 }
